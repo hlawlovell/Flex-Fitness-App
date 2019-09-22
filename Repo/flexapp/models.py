f3102808e14a4a4e689e272c365e9567f5cab081
Harry Lawrence Lovell <hlov0204@uni.sydney.edu.au>
2019-09-22 13:14:08 +1000

    Update models.py
    Added classes LogEntries, UserExercise, Exercise
        A LogEntry represents a 'Set'. This contains a specified number of 'Reps' and a specified 'Weight.' It is associated with UserExercise
    Renamed class UserCredential to UserCredentials

--- Repo/flexapp/models.py
+++ Repo/flexapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class UserCredential(models.Model):
+class UserCredentials(models.Model):
 
     email = models.EmailField(max_length=50, unique=True)
     #userName = models.CharField(max_length=50, unique=True)
@@ -16,13 +16,35 @@ class Profile(models.Model):
     name = models.CharField(max_length=50)
     birthday = models.DateField()
     height = models.DecimalField(max_digits=6,decimal_places=2)
-    Weight = models.DecimalField(max_digits=6,decimal_places=2)
+    weight = models.DecimalField(max_digits=6,decimal_places=2)
     bench = models.DecimalField(max_digits=6,decimal_places=2)
     squat = models.DecimalField(max_digits=6,decimal_places=2)
     deadlift = models.DecimalField(max_digits=6,decimal_places=2)
     flexScore = models.IntegerField()
-    user = models.ForeignKey(UserCredential, on_delete = models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(UserCredentials, on_delete = models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.user.email
 
+class User(models.Model):
+    email = models.CharField(max_length=50)
+    userName = models.CharField(max_length=50)
+    password = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.userName
+        
+class Exercise(models.Model):
+    name = models.CharField(max_length=50)
+    isModifiable = models.BooleanField(default=False)
+
+class UserExercise(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete = models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    set = models.PositiveSmallIntegerField(null=True)
+
+class LogEntries(models.Model):
+    exercise = models.ForeignKey(UserExercise, on_delete = models.CASCADE)
+    weight = models.DecimalField(max_digits=3,decimal_places=2)
+    reps = models.PositiveSmallIntegerField(null=True)
+  
