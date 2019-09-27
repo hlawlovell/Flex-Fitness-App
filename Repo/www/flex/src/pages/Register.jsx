448c49f28f9018f7e33646ac2b8d9d0ae2ac6e2b
Charles Dunstan <cdun4036@uni.sydney.edu.au>
2019-09-27 16:31:44 +1000

    added login and signup pages# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch frontend# Your branch is up to date with 'origin/frontend'.## Changes to be committed:#	modified:   .gitignore#	modified:   flex/.gitignore#	modified:   flex/README.md#	modified:   flex/package-lock.json#	modified:   flex/public/index.html#	modified:   flex/src/components/app/App.css#	modified:   flex/src/components/app/App.js#	modified:   flex/src/components/app/App.test.js#	modified:   flex/src/components/common.css#	modified:   flex/src/index.css#	modified:   flex/src/index.js#	modified:   flex/src/pages/Home.jsx#	modified:   flex/src/pages/Landing.jsx#	modified:   flex/src/pages/Login.jsx#	modified:   flex/src/pages/Profile.jsx#	new file:   flex/src/pages/Register.jsx#	modified:   flex/src/pages/Workout.jsx#

--- Repo/www/flex/src/pages/Register.jsx
+++ Repo/www/flex/src/pages/Register.jsx
@@ -3,9 +3,9 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 
-const Login = () => (
+const Register = () => (
   <div id="logForm">
-  <h1>flex</h1>
+  <h1>Sign Up</h1>
   <Form>
     <Form.Group controlId="formBasicEmail">
       <Form.Label>Email address</Form.Label>
@@ -15,13 +15,14 @@ const Login = () => (
       <Form.Label>Password</Form.Label>
       <Form.Control type="password" placeholder="Password" />
     </Form.Group>
-    <Button className="formButton" variant="primary" type="submit" href="/home">
-      Login
-    </Button>
-    <Button className="formButton" variant="primary" type="submit" href="/register">
-      register
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Confirm Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+    <Button variant="primary" type="submit" href="/home">
+      Register
     </Button>
   </Form>
   </div>
 )
-export default Login;
+export default Register;
