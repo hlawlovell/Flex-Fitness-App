1e54aa7664048b256b1accbfc90916fdac120cd8
AmyFangLittlescribe <amy.fang@littlescribe.com>
2019-10-18 15:17:04 +1100

    fix signals

--- Repo/flexapp/signals.py
+++ Repo/flexapp/signals.py
@@ -4,7 +4,11 @@ from django.dispatch import receiver
 
 from flexapp.models import Profile
 
+
+#Signal which creates profile after user registration
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
