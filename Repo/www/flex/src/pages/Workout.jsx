17edb1cb11083194078beb84672c90e5724e2a9b
charlie <cdun4036@uni.sydney.edu.au>
2019-10-24 11:47:55 +1100

    Feat(workout): dynamic date range and workouts, now need to sync with bend to pull data

--- Repo/www/flex/src/pages/Workout.jsx
+++ Repo/www/flex/src/pages/Workout.jsx
@@ -1,45 +1,20 @@
 import React from "react"
 import MetaTags from 'react-meta-tags';
-import { Container, ListGroup } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import WorkoutTracker from '../components/workoutTracker'
 import Nav from '../components/nav'
 import '../components/common.css'
-import Logo from '../components/Logo';
 
-
-const dayViewWrapStyle = {
-  'padding-top':'5%',
-  width: '90%',
-  margin:'0 auto',
-  position:'relative'
-};
-
-const workoutWrapStyle = {
-  width:'100%',
-  height:'90%',
-  position:'relatve'
-};
 
 const Workout = () => {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <MetaTags>
         <title>Workout</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet" />
       </MetaTags>
-      <div class="content"> 
-      <Container id="workoutWrap" style={workoutWrapStyle}>
-      <Logo />
-        <div id ="workouts" style={dayViewWrapStyle}>
-        <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
-        </div>
-      </Container> 
+      <div className="content"> 
+      <WorkoutTracker />
       <Container>
           <Nav  selected={"workout"}/>
       </Container>
